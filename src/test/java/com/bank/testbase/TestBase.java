1a4df2953ad205cbe3010e754bc123baf1d8c4a2
Chintan170218 <100502452+Chintan170218@users.noreply.github.com>
2022-05-13 10:25:07 +0100

    Update TestBase.java

--- src/test/java/com/bank/testbase/TestBase.java
+++ src/test/java/com/bank/testbase/TestBase.java
@@ -2,7 +2,9 @@ package com.bank.testbase;
 
 import com.bank.propertyreader.PropertyReader;
 import com.bank.utility.Utility;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 
@@ -10,12 +12,12 @@ public class TestBase extends Utility {
 
     String browser = PropertyReader.getInstance().getProperty("browser");
 
-    @BeforeMethod(alwaysRun = true)
+    @BeforeClass(alwaysRun = true)
     public void setUp(){
         selectBrowser(browser);
     }
 
-    @AfterMethod(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void tearDown(){
         closeBrowser();
     }
